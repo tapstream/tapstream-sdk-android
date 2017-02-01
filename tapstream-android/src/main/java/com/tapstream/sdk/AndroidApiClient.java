b7ef55b22ac79e8c54185d24c8476fb026a9673c
Adam Bard <adam@tapstream.com>
2017-02-01 14:32:42 -0800

    Fix javadoc issue

--- tapstream-android/src/main/java/com/tapstream/sdk/AndroidApiClient.java
+++ tapstream-android/src/main/java/com/tapstream/sdk/AndroidApiClient.java
@@ -8,6 +8,10 @@ import com.tapstream.sdk.landers.Lander;
 import com.tapstream.sdk.wordofmouth.WordOfMouth;
 
 public interface AndroidApiClient extends ApiClient {
+
+    class ServiceNotEnabled extends RuntimeException {
+        public ServiceNotEnabled(String message){ super(message); }
+    }
     /**
      * @return A WordOfMouth object used for working with Tapstream's feature
      *         of the same name.
@@ -15,12 +19,14 @@ public interface AndroidApiClient extends ApiClient {
      * @see WordOfMouth
      */
 
-    class ServiceNotEnabled extends RuntimeException {
-        public ServiceNotEnabled(String message){ super(message); }
-    }
-
     WordOfMouth getWordOfMouth() throws ServiceNotEnabled;
 
+    /**
+     * @param mainActivity Your app's main activity
+     * @param parent The parent view you want to show a popup over
+     * @param lander The Lander whose content you want to display (if it hasn't already)
+     * @throws ServiceNotEnabled
+     */
     void showLanderIfUnseen(Activity mainActivity, View parent, Lander lander) throws ServiceNotEnabled;
     void showLanderIfUnseen(Activity mainActivity, View parent, Lander lander, ILanderDelegate delegate) throws ServiceNotEnabled;
 }
